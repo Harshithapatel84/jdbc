f0927bc27ff4ec691e7938f7bf1e6bc48cc206d6
kruthika <harshithashanthraju@gmail.com>
2026-06-09 16:47:36 +0530

    jdbc add class

--- online-payment/src/main/java/com/xworkz/payment/CreditPayment.java
+++ online-payment/src/main/java/com/xworkz/payment/CreditPayment.java
@@ -1,13 +1,17 @@
 package com.xworkz.payment;
 
+import in.xworkz.payment.DebitPayment;
+
 public class CreditPayment {
     public static void main(String[] args) {
         System.out.println("payment credited");
         try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
+            Class.forName("in.xworkz.payment.DebitPayment");
         }catch(ClassNotFoundException exception)
         {
             throw new RuntimeException(exception);
         }
+        System.out.println("credited payment successfully");
+        DebitPayment.pay();
     }
 }
---
+++ online-payment/src/main/java/in/xworkz/payment/DebitPayment.java
@@ -0,0 +1,9 @@
+package in.xworkz.payment;
+
+public class DebitPayment {
+
+     public static void pay()
+    {
+        System.out.println("debited successgylly");
+    }
+}
