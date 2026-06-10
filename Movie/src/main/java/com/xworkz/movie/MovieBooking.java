f309c32dee0783e6c6f1b9c126ac226d56ff87de
kruthika <harshithashanthraju@gmail.com>
2026-06-10 23:02:25 +0530

    java & mysql connection

---
+++ Movie/src/main/java/com/xworkz/movie/MovieBooking.java
@@ -0,0 +1,54 @@
+package com.xworkz.movie;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class MovieBooking {
+    public static void main(String[] args) {
+        String url="jdbc:mysql://localhost:3306/movie_db";
+        String userName="root";
+                String password="root";
+        String sqlQuery =
+                "insert into movie_info values('Leo',4,'PVR',800)";
+
+        Connection connection = null;
+        Statement statement = null;
+
+        try {
+            connection = DriverManager.getConnection(url, userName, password);
+
+            statement = connection.createStatement();
+
+            statement.executeUpdate(sqlQuery);
+
+            System.out.println("Movie booking inserted successfully");
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+
+        } finally {
+
+            if (statement != null) {
+                try {
+                    statement.close();
+                    System.out.println("Statement closed");
+                } catch (SQLException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+
+            if (connection != null) {
+                try {
+                    connection.close();
+                    System.out.println("Connection closed");
+                } catch (SQLException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
+    }
+
+    }
+
